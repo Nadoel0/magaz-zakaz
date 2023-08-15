5062541c496563034b4af17ddc0353c10a7c37f8
Nadoel0 <irek-ignatev@mail.ru>
2023-08-15 15:48:01 +0300

    media

--- bordel/resources/views/layouts/app.blade.php
+++ bordel/resources/views/layouts/app.blade.php
@@ -9,7 +9,8 @@
 
     <title>Bordel</title>
     {{--    <title>{{ config('app.name', 'Bordel') }}</title>--}}
-    @vite(['resources/css/order_id.css', 'resources/js/order_id.js', 'resources/js/order_index.js'])
+    @vite(['resources/css/order_id.css', 'resources/css/order_index.css', 'resources/css/main.css',
+            'resources/js/order_id.js', 'resources/js/order_index.js'])
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
@@ -25,57 +26,57 @@
 </head>
 <body>
 <div id="app">
-{{--    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">--}}
-{{--        <div class="container">--}}
-{{--            <a class="navbar-brand" href="{{ route('order.index') }}">--}}
-{{--                Bordel--}}
-{{--                --}}{{--                    {{ config('app.name', 'Bordel') }}--}}
-{{--            </a>--}}
-{{--            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"--}}
-{{--                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"--}}
-{{--                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
-{{--                <span class="navbar-toggler-icon"></span>--}}
-{{--            </button>--}}
+    {{--    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">--}}
+    {{--        <div class="container">--}}
+    {{--            <a class="navbar-brand" href="{{ route('order.index') }}">--}}
+    {{--                Bordel--}}
+    {{--                --}}{{--                    {{ config('app.name', 'Bordel') }}--}}
+    {{--            </a>--}}
+    {{--            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"--}}
+    {{--                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"--}}
+    {{--                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
+    {{--                <span class="navbar-toggler-icon"></span>--}}
+    {{--            </button>--}}
 
-{{--            <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
-{{--                <!-- Left Side Of Navbar -->--}}
+    {{--            <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
+    {{--                <!-- Left Side Of Navbar -->--}}
 
-{{--                <!-- Right Side Of Navbar -->--}}
-{{--                <ul class="navbar-nav ms-auto">--}}
-{{--                    <!-- Authentication Links -->--}}
-{{--                    @guest--}}
-{{--                        @if (Route::has('login'))--}}
-{{--                            <li class="nav-item">--}}
-{{--                                <a class="nav-link" href="{{ route('login') }}">{{ __('Войти') }}</a>--}}
-{{--                            </li>--}}
-{{--                        @endif--}}
+    {{--                <!-- Right Side Of Navbar -->--}}
+    {{--                <ul class="navbar-nav ms-auto">--}}
+    {{--                    <!-- Authentication Links -->--}}
+    {{--                    @guest--}}
+    {{--                        @if (Route::has('login'))--}}
+    {{--                            <li class="nav-item">--}}
+    {{--                                <a class="nav-link" href="{{ route('login') }}">{{ __('Войти') }}</a>--}}
+    {{--                            </li>--}}
+    {{--                        @endif--}}
 
-{{--                        @if (Route::has('register'))--}}
-{{--                            <li class="nav-item">--}}
-{{--                                <a class="nav-link" href="{{ route('register') }}">{{ __('Регистрация') }}</a>--}}
-{{--                            </li>--}}
-{{--                        @endif--}}
-{{--                    @else--}}
-{{--                        <a id="navbarDropdown" class="nav-link dropdown-item" href="#" role="button"--}}
-{{--                           data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
-{{--                            {{ Auth::user()->name }}--}}
-{{--                        </a>--}}
+    {{--                        @if (Route::has('register'))--}}
+    {{--                            <li class="nav-item">--}}
+    {{--                                <a class="nav-link" href="{{ route('register') }}">{{ __('Регистрация') }}</a>--}}
+    {{--                            </li>--}}
+    {{--                        @endif--}}
+    {{--                    @else--}}
+    {{--                        <a id="navbarDropdown" class="nav-link dropdown-item" href="#" role="button"--}}
+    {{--                           data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
+    {{--                            {{ Auth::user()->name }}--}}
+    {{--                        </a>--}}
 
-{{--                        <a class="nav-link dropdown-item" href="{{ route('logout') }}"--}}
-{{--                           onclick="event.preventDefault();--}}
-{{--                                                     document.getElementById('logout-form').submit();">--}}
-{{--                            {{ __('Выйти') }}--}}
-{{--                        </a>--}}
+    {{--                        <a class="nav-link dropdown-item" href="{{ route('logout') }}"--}}
+    {{--                           onclick="event.preventDefault();--}}
+    {{--                                                     document.getElementById('logout-form').submit();">--}}
+    {{--                            {{ __('Выйти') }}--}}
+    {{--                        </a>--}}
 
-{{--                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">--}}
-{{--                            @csrf--}}
-{{--                        </form>--}}
+    {{--                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">--}}
+    {{--                            @csrf--}}
+    {{--                        </form>--}}
 
-{{--                    @endguest--}}
-{{--                </ul>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </nav>--}}
+    {{--                    @endguest--}}
+    {{--                </ul>--}}
+    {{--            </div>--}}
+    {{--        </div>--}}
+    {{--    </nav>--}}
 
     <main class="py-4">
         <div class="container">
--- bordel/vite.config.js
+++ bordel/vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
                 'resources/sass/app.scss',
                 'resources/css/app.css',
                 'resources/css/order_id.css',
+                'resources/css/order_index.css',
+                'resources/css/main.css',
                 'resources/js/order_id.js',
                 'resources/js/order_index.js',
                 'resources/js/app.js',
